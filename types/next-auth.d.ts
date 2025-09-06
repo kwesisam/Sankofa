5ef6f6d18758b7e0eaa6779ddae82e8d7932c494
--replace-all <=>
2025-09-06 06:21:25 +0100

    dd

---
+++ types/next-auth.d.ts
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+// types/next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image?: string;
+      type: string;
+    };
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    image?: string;
+    type: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    name: string;
+    email: string;
+    picture?: string;
+    type: string;
+  }
+}
+=======
+// types/next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image?: string;
+      type: string;
+    };
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    image?: string;
+    type: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    name: string;
+    email: string;
+    picture?: string;
+    type: string;
+  }
+}
+>>>>>>> 1739973 (first commit)
