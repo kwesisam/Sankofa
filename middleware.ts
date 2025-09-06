9d133014009652e6cbb0e54fa91e5a7dfc3941d7
--replace-all <=>
2025-09-06 06:19:59 +0100

    dss

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+
+import withAuth from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/signin",
+    error: "/signin",
+  },
+});
+
+export const config = {
+  matcher: [
+    "/artisan-dashboard/:path*",
+    "/user-dashboard/:path*",
+  ],
