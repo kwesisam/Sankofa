f89cf008ebcb09d9d397610b4352c7e9db4b0cc5
--replace-all <=>
2025-09-06 06:25:46 +0100

    chan

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
@@ -6,12 +6,3 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 
-=======
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-);
-
->>>>>>> 1739973 (first commit)
